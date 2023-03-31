60c10b48c93616a85c3728087cf213e3fd890b91
HiImMarc <Marc.xu92230@gmail.com>
2023-04-01 00:31:44 +0200

    Ajout de barre de recherche, ajout de messages tests,

--- myapp/src/Components/Home.js
+++ myapp/src/Components/Home.js
@@ -1,3 +1,4 @@
+import {useEffect, useState} from 'react';
 import React from 'react';
 import Logo from '../Image/Logo.png'
 import SearchBar from './SearchBar'
@@ -6,29 +7,50 @@ import SideBar from './SideBar';
 import './Home.css'
 
 function Home() {
-  return (
-    <div className="home">
-        {/* Header */}
-        <header>
-            <div className='logo'>
-                <img className='concreteLogo'src={Logo}/>
-            </div>
-            <div className='searchBar'>
-                <SearchBar/>
-            </div>
-        </header>
 
-        <div className="core">
-            <div  className="messageList">
-                <MessageList/>
+    const [messageData, setMessageData] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
+
+    useEffect(getMessageData, [])
+
+    // Filtrer le tableau de messages en fonction de la valeur de recherche
+    const filteredMessages = messageData.filter(item =>
+        item.title.toLowerCase().includes(searchInput.toLowerCase()) ||
+        item.body.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
+    function handleSearch(input){
+        setSearchInput(input);
+    }
+
+    function getMessageData(){
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then (response => response.json())
+        .then(data => setMessageData(data))
+    }
+
+    return (
+        <div className="home">
+            <header>
+                <div className='logo'>
+                    <img className='concreteLogo'src={Logo}/>
+                </div>
+                <div className='searchBar'>
+                    <SearchBar onSearch={handleSearch}/>
+                </div>
+            </header>
+
+            <div className="core">
+                <div  className="messageList">
+                    <MessageList data={filteredMessages} searchInput={searchInput}/>
+                </div>
+                <div className="sideBar">
+                    <SideBar />
+                </div>
             </div>
-            <div className="sideBar">
-                <SideBar />
-            </div>
+
         </div>
-
-    </div>
-  );
+    );
 }
 
 export default Home;
--- myapp/src/Components/SearchBar.js
+++ myapp/src/Components/SearchBar.js
@@ -1,7 +1,21 @@
-export default function SearchBar(){
+import React, { useState } from "react";
+
+export default function SearchBar(props) {
+    const [searchInput, setSearchInput] = useState("");
+
+    function handleSearch(e) {
+        setSearchInput(e.target.value);
+        props.onSearch(searchInput); // Appeler la fonction callback onSearch avec la valeur de recherche
+    }
+
     return (
-        <div>
-            SEARCHBAR
+        <div className="search-bar">
+            <input
+            type="text"
+            value={searchInput}
+            placeholder="Rechercher..."
+            onChange={handleSearch}
+            />
         </div>
-    )
+    );
 }
